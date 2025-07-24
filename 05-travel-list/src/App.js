9690e2be3b4e9168997a7b4e7fbedd59ab203811
Usama <usamakha2003@gmail.com>
2025-07-24 17:22:24 +0500

    start new mini project FAR AWAY

---
+++ 05-travel-list/src/App.js
@@ -0,0 +1,31 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h2>What do you need for your trip?</h2>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">List</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
