f369026f34a013b589e5146eb67b70170fbc8504
Usama <usamakha2003@gmail.com>
2025-07-29 21:35:20 +0500

    try to understand LITING UP STATE

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -6,11 +6,15 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((prevItems) => [...prevItems, item]);
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -19,9 +23,10 @@ export default function App() {
 function Logo() {
   return <h1>🏝️ Far Away 💼</h1>;
 }
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
     const newItem = {
@@ -30,9 +35,9 @@ function Form() {
       packed: false,
       id: Date.now(),
     };
+    onAddItems(newItem);
     setDescription("");
     setQuantity(1);
-    console.log("Adding item:", newItem);
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
@@ -54,11 +59,11 @@ function Form() {
     </form>
   );
 }
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
