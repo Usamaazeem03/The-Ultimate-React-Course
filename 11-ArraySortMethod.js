7c37c6546d5b1fc341e51c4b3b3e8f729eb68227
Usama <usamakha2003@gmail.com>
2025-10-12 17:16:55 +0500

    Array Sort Method

--- 11-ArraySortMethod.js
+++ 11-ArraySortMethod.js
@@ -1,1 +1,19 @@
 // Array Sort Method
+const arr = [9, 2, 5, 1];
+const ascending = arr.sort((a, b) => a - b);
+const descending = arr.sort((a, b) => b - a);
+console.log(ascending);
+console.log(descending);
+
+const furit = ["banana", "apple", "orange"];
+const ascenFurit = furit.sort((a, b) => a.localeCompare(b));
+console.log(ascenFurit);
+
+const great = ["hi", "hello", "hey"];
+const sortlen = great.sort((a, b) => a.length - b.length);
+
+const user = [
+  { name: "Usama", age: 25 },
+  { name: "Ali", age: 20 },
+];
+const ageSort = user.sort((a, b) => a.age - b.age);
