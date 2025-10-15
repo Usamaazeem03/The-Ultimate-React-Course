a6d78215ee9a080fffd9b5e6cc796f4991aa2e1d
Usama <usamakha2003@gmail.com>
2025-10-15 22:38:20 +0500

    Deep understanding JSX

--- 04-Section5-Working-With-Components-Props-Jsx/00-Rendering-The-Root-Component-And-Strict-Mode/pizza-menu/src/index.js
+++ 04-Section5-Working-With-Components-Props-Jsx/00-Rendering-The-Root-Component-And-Strict-Mode/pizza-menu/src/index.js
@@ -1,10 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import pizzaData from "./data";
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
 }
 
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu</h2>
+      <Pizza />
+    </div>
+  );
+}
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+  return (
+    <footer>{new Date().toLocaleTimeString()}.We'er currently open</footer>
+  );
+}
+
+// componet
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" />
+      <h2>{pizzaData[0].name}</h2>
+    </div>
+  );
+}
 // React 18V
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
