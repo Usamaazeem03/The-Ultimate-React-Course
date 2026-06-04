82bd80c33c7ff8f22992aeabc13aa29350a8dadc
Usama <usamakha2003@gmail.com>
2026-06-04 23:03:34 +0300

    today i just understand how card to creat sub.. oder work

--- 19-Section22-React-Router-With-Date-Loading/01-fast-react-pizza/src/feactures/order/CreateOrder.jsx
+++ 19-Section22-React-Router-With-Date-Loading/01-fast-react-pizza/src/feactures/order/CreateOrder.jsx
@@ -3,6 +3,7 @@ import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import { Button } from "../../ui/Button";
 import { useSelector } from "react-redux";
+import { getCart } from "../cart/cartSlice";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -40,7 +41,7 @@ function CreateOrder() {
   const isSubmitting = navigation.state === "submitting";
   const formErrors = useActionData();
   // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
 
   return (
     <div className="px-4 py-6">
